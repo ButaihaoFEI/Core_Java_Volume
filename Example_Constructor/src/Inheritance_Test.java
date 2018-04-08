82a2234a0bcb585275fda753942cb7684cbf40d1
FEI Shenghao <feishenghao@gmail.com>
2018-04-08 20:44:18 +0800

    Create Example_Constructor sample - the idea of inheritance - the idea of constructor

---
+++ Example_Constructor/src/Inheritance_Test.java
@@ -0,0 +1,12 @@
+
+public class Inheritance_Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person p = new Person("Peter");
+		Student s = new Student("Helen");
+		System.out.println("p's name is " + p.getName() );
+		System.out.println("s's name is " + s.getName() );
+	}
+
+}
